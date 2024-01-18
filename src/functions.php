6007f701a8cef27761d0ac846602607beeec3d97
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-01-18 10:37:29 +0100

    Add env helper function

---
+++ src/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Core;
+
+function env(string $key, bool|string|null $default = null): mixed
+{
+    if (func_num_args() > 1) {
+        $value = $_ENV[$key] ?? null;
+
+        if ($value === null) {
+            return $default;
+        }
+    } else {
+        $value = $_ENV[$key];
+    }
+
+    return match ($value) {
+        'true' => true,
+        'false' => false,
+        'null' => null,
+        'empty' => '',
+        default => $value,
+    };
+}
