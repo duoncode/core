9a8b38859df02eff9a4b8674133897772edb3c00
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-01-19 15:17:48 +0100

    Add emitter tests

---
+++ tests/EmitterTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Core\Tests;
+
+use Conia\Core\Emitter;
+use Conia\Core\Response;
+
+final class EmitterTest extends TestCase
+{
+    public function testSapiEmitter(): void
+    {
+        $response = Response::create($this->factory())->json([1, 2, 3])->unwrap();
+
+        $emitter = new Emitter();
+        ob_start();
+        $emitter->emit($response);
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertSame('[1,2,3]', $output);
+    }
+
+    public function testSapiStreamEmitter(): void
+    {
+        $file = "{$this->root}/public/static/image.gif";
+        $response = Response::create($this->factory())->download($file)->unwrap();
+
+        $emitter = new Emitter();
+        ob_start();
+        $emitter->emit($response);
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertSame(true, str_starts_with($output, 'GIF87a'));
+    }
+}
