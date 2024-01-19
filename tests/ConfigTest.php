ad2a612c2e18b0584caa77bbca64b2735ebf1a15
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-01-19 14:41:58 +0100

    Add AddsConfigInterface tests

---
+++ tests/ConfigTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Core\Tests;
+
+use Conia\Core\Exception\OutOfBoundsException;
+use Conia\Core\Tests\Fixtures\TestConfig;
+
+final class ConfigTest extends TestCase
+{
+    public function testGettingSetting(): void
+    {
+        $config = new TestConfig();
+
+        $config->set('album', 'Symbolic');
+        $config->set('albums', ['Symbolic', 'Leprosy']);
+        $config->set('year', 1983);
+
+        $this->assertSame(true, $config->has('album'));
+        $this->assertSame('Symbolic', $config->get('album'));
+        $this->assertSame(true, $config->has('albums'));
+        $this->assertSame(['Symbolic', 'Leprosy'], $config->get('albums'));
+        $this->assertSame(true, $config->has('year'));
+        $this->assertSame(1983, $config->get('year'));
+        $this->assertSame(false, $config->has('does-not-exist'));
+    }
+
+    public function testGettingDefaultValue(): void
+    {
+
+        $config = new TestConfig();
+
+        $this->assertSame('default-value', $config->get('does-not-exist', 'default-value'));
+    }
+
+    public function testThrowingException(): void
+    {
+        $this->throws(OutOfBoundsException::class);
+
+        $config = new TestConfig();
+
+        $config->get('does-not-exist');
+    }
+}
