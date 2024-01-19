eb4efa0188939a4f28e9f039171ecfef61274a41
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-01-19 14:56:47 +0100

    Add function tests

---
+++ tests/FunctionTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Core\Tests;
+
+use function Conia\Core\env;
+
+final class FunctionTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        global $_ENV;
+        $_ENV = [];
+    }
+
+    public function testFunctionEnvGetValue(): void
+    {
+        $_ENV['TEST'] = '1983';
+
+        $this->assertSame('1983', env('TEST'));
+    }
+
+    public function testFunctionEnvGetDefaultValue(): void
+    {
+        $this->assertSame('2001', env('TEST', '2001'));
+    }
+
+    public function testFunctionEnvTypeCasting(): void
+    {
+        $_ENV['TEST'] = 'true';
+        $this->assertSame(true, env('TEST'));
+        $_ENV['TEST'] = 'True';
+        $this->assertSame(true, env('TEST'));
+        $_ENV['TEST'] = 'TRUE';
+        $this->assertSame(true, env('TEST'));
+        $_ENV['TEST'] = 'tRUe';
+        $this->assertSame(true, env('TEST'));
+
+        $_ENV['TEST'] = 'false';
+        $this->assertSame(false, env('TEST'));
+        $_ENV['TEST'] = 'False';
+        $this->assertSame(false, env('TEST'));
+        $_ENV['TEST'] = 'FALSE';
+        $this->assertSame(false, env('TEST'));
+        $_ENV['TEST'] = 'faLsE';
+        $this->assertSame(false, env('TEST'));
+
+        $_ENV['TEST'] = 'null';
+        $this->assertSame(null, env('TEST'));
+        $_ENV['TEST'] = 'Null';
+        $this->assertSame(null, env('TEST'));
+        $_ENV['TEST'] = 'NULL';
+        $this->assertSame(null, env('TEST'));
+        $_ENV['TEST'] = 'nUlL';
+        $this->assertSame(null, env('TEST'));
+    }
+}
